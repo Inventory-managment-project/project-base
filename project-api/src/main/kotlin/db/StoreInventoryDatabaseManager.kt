f26fb1685efe186c73498f104be8871eb5c52b1c
SlemimanPzz <emilianoa@ciencias.unam.mx>
2025-03-12 15:49:56 -0600

    StoreInventoryDatabaseManager Interface

--- project-api/src/main/kotlin/db/StoreInventoryDatabaseManager.kt
+++ project-api/src/main/kotlin/db/StoreInventoryDatabaseManager.kt
@@ -1,7 +1,5 @@
 package mx.unam.fciencias.ids.eq1.db
 
-import mx.unam.fciencias.ids.eq1.model.store.CreateStoreRequest
-import mx.unam.fciencias.ids.eq1.model.user.User
 import org.jetbrains.exposed.sql.Database
 
 interface StoreInventoryDatabaseManager {
