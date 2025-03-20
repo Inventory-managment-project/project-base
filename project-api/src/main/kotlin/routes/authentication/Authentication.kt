7c242dbaf10e3c3d2a23614d69aa38dc850ec9b9
SlemimanPzz <emilianoa@ciencias.unam.mx>
2025-03-20 14:40:26 -0600

    Removes Cookie

--- project-api/src/main/kotlin/routes/authentication/Authentication.kt
+++ project-api/src/main/kotlin/routes/authentication/Authentication.kt
@@ -2,16 +2,18 @@ package mx.unam.fciencias.ids.eq1.routes.authentication
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.http.content.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.date.*
 import mx.unam.fciencias.ids.eq1.model.user.CreateUserRequest
 import mx.unam.fciencias.ids.eq1.model.user.User
 import mx.unam.fciencias.ids.eq1.security.hashing.HashingService
 import mx.unam.fciencias.ids.eq1.security.hashing.SaltedHash
 import mx.unam.fciencias.ids.eq1.security.request.AuthRequest
-import mx.unam.fciencias.ids.eq1.security.request.ValidateRequest
 import mx.unam.fciencias.ids.eq1.security.token.TokenClaim
 import mx.unam.fciencias.ids.eq1.security.token.TokenConfig
 import mx.unam.fciencias.ids.eq1.security.tokens.TokenProvider
@@ -57,7 +59,7 @@ fun Application.authenticationRouting(
                     call.response.cookies.append(
                         name = "token",
                         value = token,
-                        encoding =  CookieEncoding.BASE64_ENCODING
+                        encoding =  CookieEncoding.BASE64_ENCODING,
                     )
                     call.respond(HttpStatusCode.OK, mapOf("token" to token))
                 } else {
@@ -84,18 +86,24 @@ fun Application.authenticationRouting(
                 }
             }
         }
-        route("validate") {
-            post {
-                val request = call.receive<ValidateRequest>()
-                val token = request.token
-
-                if (token.isNullOrBlank()) {
-                    call.respond(HttpStatusCode.Unauthorized, mapOf("message" to "No token provided"))
-                    return@post
+        authenticate("auth-jwt") {
+            route("validate") {
+                post {
+                    call.respond(HttpStatusCode.OK, mapOf("message" to "Token is valid"))
                 }
-
-                call.respond(HttpStatusCode.OK, mapOf("message" to "Token is valid"));
+            }
+            route("logout") {
+                post {
+                    call.response.cookies.append(
+                        name = "token",
+                        value = "",
+                        expires = GMTDate.START
+                    )
+                    call.principal<JWTPrincipal>()
+                    call.respond(HttpStatusCode.OK, mapOf("message" to "Logged out"))
+                }
             }
         }
+
     }
 }
