f76147eff1c05af3f62413dd931dfdfb193b26bd
Yael Lozano <lozanoy6@gmail.com>
2025-03-31 23:01:22 -0600

    middleware for dev env

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,7 +1,12 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+//production - docker
 const API_AUTH_URL = "http://172.18.0.4:8080/validate";
+
+//dev 
+//const API_AUTH_URL = "http://localhost:8080/validate";
+
 
 function decodeBase64(str: string) {
   return Buffer.from(str, "base64").toString("utf-8");
