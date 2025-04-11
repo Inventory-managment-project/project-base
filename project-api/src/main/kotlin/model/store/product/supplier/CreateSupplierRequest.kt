00cbfcbf4527440ee3d187ea9a46a060b06c0562
kievs <angeles_ortacas17@gmail.com>
2025-04-10 22:09:22 -0600

    Serializable added in Create and Update Supplier Request

--- project-api/src/main/kotlin/model/store/product/supplier/CreateSupplierRequest.kt
+++ project-api/src/main/kotlin/model/store/product/supplier/CreateSupplierRequest.kt
@@ -1,5 +1,8 @@
 package mx.unam.fciencias.ids.eq1.model.store.product.supplier
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CreateSupplierRequest (
     val name: String,
     val contactName: String,
