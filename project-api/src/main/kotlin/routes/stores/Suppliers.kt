4457abe4c72a90db65977b95beff11e836233142
SlemimanPzz <emilianoa@ciencias.unam.mx>
2025-06-11 00:59:13 -0700

    Basic Providers routes

---
+++ project-api/src/main/kotlin/routes/stores/Suppliers.kt
@@ -0,0 +1,41 @@
+package mx.unam.fciencias.ids.eq1.routes.stores
+
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.request.receive
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.route
+import mx.unam.fciencias.ids.eq1.model.store.product.supplier.CreateSupplierRequest
+import mx.unam.fciencias.ids.eq1.model.store.product.supplier.repository.SupplierRepository
+import org.koin.ktor.ext.inject
+import io.ktor.server.response.*
+import mx.unam.fciencias.ids.eq1.routes.getStoreIdOrBadRequest
+import org.koin.core.parameter.parametersOf
+
+fun Route.suppliersRoutes() {
+    route("/{storeId}/suppliers") {
+        get {
+            val storeId = call.getStoreIdOrBadRequest() ?: return@get
+            val supplierRepository by call.inject<SupplierRepository> { parametersOf(storeId)}
+            val suppliers = supplierRepository.getAll()
+            call.respond(HttpStatusCode.OK, suppliers)
+        }
+        post {
+            val storeId = call.getStoreIdOrBadRequest() ?: return@post
+            val createSupplierRequest = call.receive<CreateSupplierRequest>()
+            val supplierRepository by call.inject<SupplierRepository> { parametersOf(storeId)}
+            supplierRepository.add(createSupplierRequest)
+            call.respond(HttpStatusCode.Created)
+        }
+        route("/{supplierId}") {
+            get {
+                val storeId = call.getStoreIdOrBadRequest() ?: return@get
+                val supplierRepository by call.inject<SupplierRepository> { parametersOf(storeId)}
+            }
+            route("/suppliesProduct") {
+
+            }
+        }
+    }
+}
