c6057a8c2d8dbda5bcf78a6bbe074b250dad9664
SlemimanPzz <emilianoa@ciencias.unam.mx>
2025-06-11 13:44:13 -0700

    New routes implemented in Suppliers

--- project-api/src/main/kotlin/routes/stores/Suppliers.kt
+++ project-api/src/main/kotlin/routes/stores/Suppliers.kt
@@ -32,9 +32,57 @@ fun Route.suppliersRoutes() {
             get {
                 val storeId = call.getStoreIdOrBadRequest() ?: return@get
                 val supplierRepository by call.inject<SupplierRepository> { parametersOf(storeId)}
+                val supplierID = call.parameters["supplierId"]
+                if (supplierID == null) {
+                    call.respond(HttpStatusCode.NotFound)
+                } else {
+                    try {
+                        val supplier = supplierRepository.getById(supplierID.toInt())
+                        if (supplier != null) {
+                            call.respond(HttpStatusCode.OK, supplier)
+                        } else {
+                            call.respond(HttpStatusCode.NotFound)
+                        }
+                    } catch (_ : Exception) {
+                        call.respond(HttpStatusCode.NotFound)
+                    }
+                }
             }
             route("/suppliesProduct/{productId}") {
+                get {
+                    val storeId = call.getStoreIdOrBadRequest() ?: return@get
+                    val supplierRepository by call.inject<SupplierRepository> { parametersOf(storeId)}
+                    val supplierID = call.parameters["supplierId"]
+                    val productId = call.parameters["productId"]
+                   if (supplierID == null || productId == null) {
+                       call.respond(HttpStatusCode.NotFound)
+                       return@get
+                   }
+                    try {
+                        val product = supplierRepository.suppliesProducts(supplierID.toInt(), productId.toInt())
+                        call.respond(HttpStatusCode.OK, product)
+                    } catch ( _ : Exception) {
+                        call.respond(HttpStatusCode.NotFound)
+                    }
+                }
 
+                post {
+                    val storeId = call.getStoreIdOrBadRequest() ?: return@post
+                    val supplierRepository by call.inject<SupplierRepository> { parametersOf(storeId)}
+                    val supplierID = call.parameters["supplierId"]
+                    val productId = call.parameters["productId"]
+                    if (supplierID == null || productId == null) {
+                        call.respond(HttpStatusCode.NotFound)
+                        return@post
+                    }
+                    try {
+                        val wasAdded = supplierRepository.addProductSupply(supplierID.toInt(), productId.toInt())
+                        call.respond(HttpStatusCode.OK, mapOf("added" to wasAdded))
+                    }
+                    catch (_ : Exception) {
+                        call.respond(HttpStatusCode.NotFound)
+                    }
+                }
             }
         }
     }
